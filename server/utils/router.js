b20d0374080353928795f9a09ee83bca4137a4b9
jumpei-0ga <jumpei.ogasawara@konicaminolta.com>
2025-02-27 22:18:31 +0900

    fix: ポケモン追加機能のバグを修正

--- server/utils/router.js
+++ server/utils/router.js
@@ -77,11 +77,24 @@ router.post("/trainer/:trainerName/pokemon", async (req, res, next) => {
   try {
     const { trainerName } = req.params;
     // TODO: リクエストボディにポケモン名が含まれていなければ400を返す
+    const trainer = await findTrainer(trainerName); // 先にトレーナーを取得する処理を実装する必要があります
+    if (!("name" in req.body && req.body.name.length > 0))
+      return res.sendStatus(400);
     const pokemon = await findPokemon(req.body.name);
     // TODO: 削除系 API エンドポイントを利用しないかぎりポケモンは保持する
-    const trainer = await findTrainer(trainerName);
-    const pokemons = [...trainer.pokemons, pokemon];
-    const result = await upsertTrainer(trainerName, { pokemons: [pokemons] });
+    const {
+      order,
+      name,
+      sprites: { front_default },
+    } = pokemon;
+    trainer.pokemons.push({
+      id: new Date().getTime(), // 何か衝突しない値の生成方法であればなんでもいいです
+      nickname: "",
+      order,
+      name,
+      sprites: { front_default },
+    });
+    const result = await upsertTrainer(trainerName, trainer);
     res.status(result["$metadata"].httpStatusCode).send(result);
   } catch (err) {
     next(err);
